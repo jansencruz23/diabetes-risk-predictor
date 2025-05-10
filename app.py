f1972a241fbce5b2466172d59789e8358446053f
Jansen Cruz <jansencruz12345@gmail.com>
2025-05-10 17:21:13 +0800

    Refactor code

--- app.py
+++ app.py
@@ -2,45 +2,49 @@ import streamlit as st
 import pickle
 from funcs import predict_risk_category
 
+# load model and scaler
 with open('model_and_scaler.pkl', 'rb') as f:
     scaler, model = pickle.load(f)
 
-result, conf = '', ''
-
+# page title
 st.title("Diabetes Risk Assessment Form Using :blue[SVM Machine Learning]")
 
-form = st.form('form')
-age = form.number_input('Age', step=1, min_value=0, max_value=120)
-bmi = form.number_input('BMI', min_value=0.0, max_value=200.0)
-gender = form.radio(
-    'Gender',
-    ['Male', 'Female']
-)
-waist = form.number_input('Waist in cm', step=0.25, min_value=0.0)
+# forms
+with st.form('risk_form'):
+    age = st.number_input('Age', step=1, min_value=0, max_value=120)
+    bmi = st.number_input('BMI', min_value=0.0, max_value=200.0)
+    gender = st.radio('Gender', ['Male', 'Female'])
+    waist = st.number_input('Waist in cm', step=0.25, min_value=0.0)
 
-phy_act = form.radio(
-    'Do you usually have daily at least 30 minutes of physical activity at work and/or during leisure time (including normal daily activity)?',
-    ['Yes', 'No']
-)
-diet = form.radio(
-    'How often do you eat vegetables, fruit, or berries?',
-    ['Every day', 'Not every day']
-)
-bp_med = form.radio(
-    'Have you ever taken medication for high blood pressure on regular basis?',
-    ['Yes', 'No']
-)
-high_glu = form.radio(
-    'Have you ever been found to have high blood glucose (e.g. in a health examination, during an illness, during pregnancy)?',
-    ['Yes', 'No']
-)
-fam_mem = form.radio(
-    'Have any of the members of your immediate family or other relatives been diagnosed with diabetes (type 1 or type 2)?',
-    ['Yes: grandparents, aunt, uncle or first cousin (but no own parent, brother, sister, or child)', 'Yes: parent, brother, sister, or own child', 'No']
-)
-submit = form.form_submit_button('Submit')
+    phy_act = st.radio(
+        'Do you have at least 30 minutes of daily physical activity?',
+        ['Yes', 'No']
+    )
+    diet = st.radio(
+        'How often do you eat vegetables, fruit, or berries?',
+        ['Every day', 'Not every day']
+    )
+    bp_med = st.radio(
+        'Have you taken medication for high blood pressure regularly?',
+        ['Yes', 'No']
+    )
+    high_glu = st.radio(
+        'Have you ever had high blood glucose?',
+        ['Yes', 'No']
+    )
+    fam_mem = st.radio(
+        'Family history of diabetes?',
+        [
+            'Yes: grandparents, aunt, uncle or first cousin',
+            'Yes: parent, sibling, or child',
+            'No'
+        ]
+    )
 
-if submit:
+    submitted = st.form_submit_button('Submit')
+
+# after submission
+if submitted:
     age_scr = 0 if age < 45 else 2 if age >= 45 and age <= 54 else 3 if age >= 55 and age <= 64 else 4
     bmi_scr = 0 if bmi < 25 else 1 if bmi >= 25 and bmi <= 30 else 3
     m_waist_scr = 0 if gender == 'Female' else 0 if waist < 94 else 3 if waist >= 94 and waist <= 102 else 4
@@ -51,11 +55,14 @@ if submit:
     high_glu_scr = 0 if high_glu == 'No' else 5
     fam_mem_scr = 0 if fam_mem == 'No' else 5 if fam_mem == 'Yes: parent, brother, sister, or own child' else 3
 
-    feats = [bmi_scr, phy_act_scr, diet_scr, bp_med_scr, high_glu_scr, fam_mem_scr, m_waist_scr, f_waist_scr, age_scr]
+    features = [
+        bmi_scr, phy_act_scr, diet_scr, bp_med_scr,
+        high_glu_scr, fam_mem_scr, m_waist_scr, f_waist_scr, age_scr
+    ]
     
-    result, conf = predict_risk_category(model, scaler, *feats)
+    risk, conf = predict_risk_category(model, scaler, *features)
 
-container = st.container(border=True)
-container.write("### Predictions")
-container.write(f'Risk: {result}')
-container.write(f'Confidence: {conf}')
+    with st.container(border=True):
+        st.write("### Predictions")
+        st.write(f"Risk: {risk}")
+        st.write(f"Confidence: {conf}")
